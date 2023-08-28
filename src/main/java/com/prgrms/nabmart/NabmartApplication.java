03ffa593c0db941749f238cca033ffcd7dfb143b
baejunil <bjo6300@naver.com>
2023-08-28 22:12:05 +0900

    NAYB-2 chore : 프로젝트 생성

---
+++ src/main/java/com/prgrms/nabmart/NabmartApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.nabmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NabmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NabmartApplication.class, args);
+	}
+
+}
