8fc6bc5e40bfbeafb3b6fc593d5041b0536a2ffb
seongHyun-Min <steve6133@naver.com>
2023-08-31 15:51:47 +0900

    feat : Coupon Entity 생성

---
+++ src/main/java/com/prgrms/nabmart/domain/coupon/Coupon.java
@@ -0,0 +1,39 @@
+package com.prgrms.nabmart.domain.coupon;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long CouponId;
+
+    @Column(nullable = false)
+    private Integer discount;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Integer minOrderPrice;
+
+    @Column(nullable = false)
+    private LocalDate endAt;
+}
