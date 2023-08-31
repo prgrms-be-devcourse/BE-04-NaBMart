751933618c33a1bf390a2c5ae29fa32005f0dfe4
baejunil <bjo6300@naver.com>
2023-08-31 14:56:33 +0900

    [NAYB-60] feat : 장바구니 도메인 구현

---
+++ src/main/java/com/prgrms/nabmart/domain/cart/Cart.java
@@ -0,0 +1,33 @@
+package com.prgrms.nabmart.domain.cart;
+
+import com.prgrms.nabmart.domain.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "cart")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Cart(User user) {
+        this.user = user;
+    }
+}
