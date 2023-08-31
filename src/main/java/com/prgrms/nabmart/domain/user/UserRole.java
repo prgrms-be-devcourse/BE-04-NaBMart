1df97199e355ee099c1bd4eef156add1147ac0a7
hseong3243 <hseong3243@gmail.com>
2023-08-31 14:15:19 +0900

    [NABY-53] feat: 유저 도메인 구현

---
+++ src/main/java/com/prgrms/nabmart/domain/user/UserRole.java
@@ -0,0 +1,24 @@
+package com.prgrms.nabmart.domain.user;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public enum UserRole {
+    ROLE_USER(Constants.ROLE_USER, List.of(Constants.ROLE_USER)),
+    ROLE_ADMIN(Constants.ROLE_ADMIN, List.of(Constants.ROLE_ADMIN, Constants.ROLE_USER));
+
+    private final String value;
+    private final List<String> authorities;
+
+    UserRole(String value, List<String> authorities) {
+        this.value = value;
+        this.authorities = authorities;
+    }
+
+    private static class Constants {
+        private static final String ROLE_USER = "ROLE_USER";
+        private static final String ROLE_ADMIN = "ROLE_ADMIN";
+    }
+}
