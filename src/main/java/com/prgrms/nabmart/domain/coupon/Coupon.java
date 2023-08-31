2a7da9cc10cc1c2e83210d272c504b18d2eb1194
seongHyun-Min <steve6133@naver.com>
2023-08-31 16:35:54 +0900

    feat : Coupon : Order 1:1 매핑 mappedBy

--- src/main/java/com/prgrms/nabmart/domain/coupon/Coupon.java
+++ src/main/java/com/prgrms/nabmart/domain/coupon/Coupon.java
@@ -1,11 +1,13 @@
 package com.prgrms.nabmart.domain.coupon;
 
 import com.prgrms.nabmart.domain.BaseTimeEntity;
+import com.prgrms.nabmart.domain.order.Order;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -15,9 +17,9 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Coupon extends BaseTimeEntity {
 
     @Id
@@ -37,4 +39,7 @@ public class Coupon extends BaseTimeEntity {
 
     @Column(nullable = false)
     private LocalDate endAt;
+
+    @OneToOne(mappedBy = "coupon")
+    private Order order;
 }
