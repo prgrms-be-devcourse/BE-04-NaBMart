4addf86947971d0f179854ac5e26280e3c135710
baejunil <bjo6300@naver.com>
2023-09-01 15:17:42 +0900

    [NAYB-21] refactor : 장바구니의 구매자 관계 OneToOne으로 변경

--- src/main/java/com/prgrms/nabmart/domain/cart/Cart.java
+++ src/main/java/com/prgrms/nabmart/domain/cart/Cart.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -24,7 +24,7 @@ public class Cart extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long cartId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
