05bdd6b498c65d785a5cc47894f8de0b9ca46c99
seongHyun-Min <steve6133@naver.com>
2023-09-01 17:19:35 +0900

    feat : Coupon Exception 추가

---
+++ src/main/java/com/prgrms/nabmart/domain/coupon/exception/CouponException.java
@@ -0,0 +1,8 @@
+package com.prgrms.nabmart.domain.coupon.exception;
+
+public class CouponException extends RuntimeException {
+
+    public CouponException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/prgrms/nabmart/domain/coupon/exception/CouponValidationException.java
@@ -0,0 +1,8 @@
+package com.prgrms.nabmart.domain.coupon.exception;
+
+public class CouponValidationException extends CouponException {
+
+    public CouponValidationException(String message) {
+        super(message);
+    }
+}
