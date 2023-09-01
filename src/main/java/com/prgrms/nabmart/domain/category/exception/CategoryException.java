b807886d23a90e27b37c0e73d5e4d8409e4ad89a
Seongju <lsj40413@gmail.com>
2023-09-01 21:50:36 +0900

    [NAYB-46] feat : Category custom exception 생성
    - CategoryException: Category 관련 최상위 Exception
    - DuplicateCategoryNameException: 카테고리명 중복 Exception

---
+++ src/main/java/com/prgrms/nabmart/domain/category/exception/CategoryException.java
@@ -0,0 +1,8 @@
+package com.prgrms.nabmart.domain.category.exception;
+
+public class CategoryException extends RuntimeException {
+
+    public CategoryException(String message) {
+        super(message);
+    }
+}
