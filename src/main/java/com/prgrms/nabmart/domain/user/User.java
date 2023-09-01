66ca19585ec3c2e08c551cc4d9674335b0a8459c
hseong3243 <hseong3243@gmail.com>
2023-09-01 22:29:00 +0900

    [NAYB-4] feat: `User` 생성시 닉네임 검증 추가

--- src/main/java/com/prgrms/nabmart/domain/user/User.java
+++ src/main/java/com/prgrms/nabmart/domain/user/User.java
@@ -1,7 +1,7 @@
 package com.prgrms.nabmart.domain.user;
 
+import com.prgrms.nabmart.domain.user.exception.InvalidNicknameException;
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +11,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 public class User {
 
+    private static final int NICKNAME_LENGTH = 200;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
@@ -18,17 +20,28 @@ public class User {
     @Column(nullable = false)
     private String nickname;
 
+    @Column(nullable = false)
     private String provider;
+
+    @Column(nullable = false)
     private String providerId;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private UserRole userRole;
 
     public User(String nickname, String provider, String providerId, UserRole userRole) {
+        validateNickname(nickname);
         this.nickname = nickname;
         this.provider = provider;
         this.providerId = providerId;
         this.userRole = userRole;
+    }
+
+    private void validateNickname(String nickname) {
+        if (nickname.length() > NICKNAME_LENGTH) {
+            throw new InvalidNicknameException("사용할 수 없는 닉네임입니다.");
+        }
     }
 }
 
---
+++ src/test/java/com/prgrms/nabmart/domain/user/UserTest.java
@@ -0,0 +1,44 @@
+package com.prgrms.nabmart.domain.user;
+
+import com.prgrms.nabmart.domain.user.exception.InvalidNicknameException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class UserTest {
+
+    @Nested
+    @DisplayName("User 생성 시")
+    class NewUserTest {
+
+        @ParameterizedTest
+        @CsvSource({
+                "가나다", "abc", "123", "가나다123", "abc123", "가나다abc123", "가나다abc123!@#"
+        })
+        @DisplayName("성공: 유효한 User 닉네임")
+        void success(String validNickname) {
+            //given
+            //when
+            User newUser = new User(validNickname, "provider", "123", UserRole.ROLE_USER);
+
+            //then
+            assertThat(newUser.getNickname()).isEqualTo(validNickname);
+        }
+
+        @Test
+        @DisplayName("예외: 200자를 초과한 닉네임")
+        void throwExceptionWhenInvalidNickname() {
+            //given
+            String invalidNickname = "a".repeat(201);
+            //when
+            //then
+            assertThatThrownBy(() -> new User(invalidNickname, "provider", "123", UserRole.ROLE_USER))
+                    .isInstanceOf(InvalidNicknameException.class);
+        }
+    }
+}
