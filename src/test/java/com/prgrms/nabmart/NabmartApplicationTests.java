4145b84844769e6aa9bdda72ed8a4a6d29987578
hseong3243 <hseong3243@gmail.com>
2023-09-01 17:25:10 +0900

    [NAYB-4] test: `contextLoads` 테스트를 위한 환경 변수 추가

--- src/test/java/com/prgrms/nabmart/NabmartApplicationTests.java
+++ src/test/java/com/prgrms/nabmart/NabmartApplicationTests.java
@@ -1,10 +1,24 @@
 package com.prgrms.nabmart;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Properties;
+
 @SpringBootTest
 class NabmartApplicationTests {
+
+	@BeforeAll
+	static void beforeAll() {
+		Properties properties = System.getProperties();
+		properties.setProperty("ISSUER", "issuer");
+		properties.setProperty("CLIENT_SECRET", "clientSecret");
+		properties.setProperty("NAVER_CLIENT_ID", "naverClientId");
+		properties.setProperty("NAVER_CLIENT_SECRET", "naverClientSecret");
+		properties.setProperty("KAKAO_CLIENT_ID", "kakaoClientId");
+		properties.setProperty("KAKAO_CLIENT_SECRET", "kakaoClientSecret");
+	}
 
 	@Test
 	void contextLoads() {
