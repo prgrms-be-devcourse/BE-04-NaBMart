44dbef0a4a97db3e42658c1085c9347292785ab9
Seongju <lsj40413@gmail.com>
2023-09-01 21:48:46 +0900

    [NAYB-46] feat : existsByName() 조회 메서드 추가
    - 카테고리 추가 시, 존재여부 판단하기 위해 생성

--- src/main/java/com/prgrms/nabmart/domain/category/repository/MainCategoryRepository.java
+++ src/main/java/com/prgrms/nabmart/domain/category/repository/MainCategoryRepository.java
@@ -4,4 +4,6 @@ import com.prgrms.nabmart.domain.category.MainCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MainCategoryRepository extends JpaRepository<MainCategory, Long> {
+
+    boolean existsByName(String name);
 }
