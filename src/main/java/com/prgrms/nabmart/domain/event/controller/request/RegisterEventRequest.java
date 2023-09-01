453c3b8f99610e8c27007463a6cfcf263e270993
Sunny <rosa7536@naver.com>
2023-09-01 18:22:12 +0900

    [NAYB-37] refactor : RegisterEventRequest validation 변경

--- src/main/java/com/prgrms/nabmart/domain/event/controller/request/RegisterEventRequest.java
+++ src/main/java/com/prgrms/nabmart/domain/event/controller/request/RegisterEventRequest.java
@@ -1,10 +1,12 @@
 package com.prgrms.nabmart.domain.event.controller.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public record RegisterEventRequest(
-    @NotNull String title,
-    @NotNull String description
+    @NotBlank(message = "이벤트 제목은 필수 입력 항목입니다.")
+    String title,
+    @NotBlank(message = "이벤트 설명은 필수 입력 항목입니다.")
+    String description
 ) {
 
 }
