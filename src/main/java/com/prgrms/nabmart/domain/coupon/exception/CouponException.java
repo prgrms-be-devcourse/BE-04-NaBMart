b01732cd65ae5a077b317e7fc41e117b7537e36b
seongHyun-Min <steve6133@naver.com>
2023-09-01 22:36:43 +0900

    refactor : Exception 추상 클래스 변경

--- src/main/java/com/prgrms/nabmart/domain/coupon/exception/CouponException.java
+++ src/main/java/com/prgrms/nabmart/domain/coupon/exception/CouponException.java
@@ -1,6 +1,6 @@
 package com.prgrms.nabmart.domain.coupon.exception;
 
-public class CouponException extends RuntimeException {
+public abstract class CouponException extends RuntimeException {
 
     public CouponException(String message) {
         super(message);
