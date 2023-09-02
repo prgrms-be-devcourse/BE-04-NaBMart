9fee016ed3bbb483ca87d4cf2dd6afade131e4f2
Sunny <rosa7536@naver.com>
2023-09-02 17:55:29 +0900

    [NAYB-37] feat : controller, service, dto 테스트

--- src/main/java/com/prgrms/nabmart/domain/event/repository/EventRepository.java
+++ src/main/java/com/prgrms/nabmart/domain/event/repository/EventRepository.java
@@ -4,12 +4,5 @@ import com.prgrms.nabmart.domain.event.domain.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
-
-    // CREATE
-    Event save(Event event);
-
-    // READ
-
-    // DELETE
-
+    
 }
