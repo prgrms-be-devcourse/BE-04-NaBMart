a40b03e46baf623fe7575918e7d9c73d3418e7e0
pushedrumex <pushedrumex@naver.com>
2023-09-04 19:55:24 +0900

    [NAYB-28]test: order 와 user 의 fixture 를 생성한다

---
+++ src/test/java/com/prgrms/nabmart/domain/order/support/OrderFixture.java
@@ -0,0 +1,37 @@
+package com.prgrms.nabmart.domain.order.support;
+
+import com.prgrms.nabmart.domain.order.Order;
+import com.prgrms.nabmart.domain.order.OrderStatus;
+import com.prgrms.nabmart.domain.user.User;
+
+public class OrderFixture {
+
+    private static final int PRICE = 10000;
+
+    public static Order getPendingOrder(long orderId, User user) {
+        return Order.builder()
+                .orderId(orderId)
+                .price(PRICE)
+                .status(OrderStatus.PENDING)
+                .user(user)
+                .build();
+    }
+
+    public static Order getDeliveringOrder(long orderId, User user) {
+        return Order.builder()
+                .orderId(orderId)
+                .price(PRICE)
+                .status(OrderStatus.DELIVERING)
+                .user(user)
+                .build();
+    }
+
+    public static Order getCompletedOrder(long orderId, User user) {
+        return Order.builder()
+                .orderId(orderId)
+                .price(PRICE)
+                .status(OrderStatus.COMPLETED)
+                .user(user)
+                .build();
+    }
+}
