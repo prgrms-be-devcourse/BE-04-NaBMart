a76ab84170f24b503382b13d87d878317f442f8e
Seongju <lsj40413@gmail.com>
2023-09-04 16:25:11 +0900

    [NAYB-47] refactor : 데이터 여부 조회 메서드명 수정(existsBy)

--- src/main/java/com/prgrms/nabmart/domain/category/repository/SubCategoryRepository.java
+++ src/main/java/com/prgrms/nabmart/domain/category/repository/SubCategoryRepository.java
@@ -1,9 +1,10 @@
 package com.prgrms.nabmart.domain.category.repository;
 
+import com.prgrms.nabmart.domain.category.MainCategory;
 import com.prgrms.nabmart.domain.category.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
 
-    boolean existsByName(String name);
+    boolean existsByMainCategoryAndName(MainCategory mainCategory, String name);
 }
