e3286e2155a37799ec24d652595d8fa8d538092a
Seongju <lsj40413@gmail.com>
2023-09-04 16:28:44 +0900

    [NAYB-47] test : 소카테고리 저장 로직

--- src/test/java/com/prgrms/nabmart/domain/category/service/CategoryServiceTest.java
+++ src/test/java/com/prgrms/nabmart/domain/category/service/CategoryServiceTest.java
@@ -1,15 +1,21 @@
 package com.prgrms.nabmart.domain.category.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.prgrms.nabmart.domain.category.MainCategory;
+import com.prgrms.nabmart.domain.category.SubCategory;
 import com.prgrms.nabmart.domain.category.exception.DuplicateCategoryNameException;
 import com.prgrms.nabmart.domain.category.repository.MainCategoryRepository;
+import com.prgrms.nabmart.domain.category.repository.SubCategoryRepository;
 import com.prgrms.nabmart.domain.category.service.request.RegisterMainCategoryCommand;
+import com.prgrms.nabmart.domain.category.service.request.RegisterSubCategoryCommand;
+import com.prgrms.nabmart.global.fixture.CategoryFixture;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -23,6 +29,9 @@ public class CategoryServiceTest {
 
     @Mock
     private MainCategoryRepository mainCategoryRepository;
+
+    @Mock
+    private SubCategoryRepository subCategoryRepository;
 
     @InjectMocks
     private CategoryService categoryService;
@@ -58,6 +67,49 @@ public class CategoryServiceTest {
             assertThatThrownBy(() -> categoryService.saveMainCategory(command))
                 .isInstanceOf(DuplicateCategoryNameException.class);
             verify(mainCategoryRepository, times(1)).existsByName("TestCategory");
+        }
+    }
+
+
+    @Nested
+    @DisplayName("saveSubCategory 메서드 실행 시")
+    class SaveSubCategoryTests {
+
+        RegisterSubCategoryCommand registerSubCategoryCommand = CategoryFixture.registerSubCategoryCommand();
+        MainCategory mainCategory = CategoryFixture.mainCategory();
+        SubCategory subCategory = CategoryFixture.subCategory(mainCategory);
+
+        @Test
+        @DisplayName("성공")
+        public void success() {
+            // Given
+            when(mainCategoryRepository.findById(anyLong())).thenReturn(Optional.of(mainCategory));
+            when(subCategoryRepository.existsByMainCategoryAndName(any(), any())).thenReturn(
+                false);
+            when(subCategoryRepository.save(any())).thenReturn(subCategory);
+
+            // When
+            categoryService.saveSubCategory(registerSubCategoryCommand);
+
+            // Then
+
+            verify(subCategoryRepository, times(1)).save(any(SubCategory.class));
+            verify(mainCategoryRepository, times(1)).findById(anyLong());
+        }
+
+        @Test
+        @DisplayName("예외: MainCategory 내 이미 존재하는 SubCategory 이름")
+        public void throwExceptionWhenNameIsDuplicated() {
+            // Given
+            when(mainCategoryRepository.findById(anyLong())).thenReturn(Optional.of(mainCategory));
+            when(subCategoryRepository.existsByMainCategoryAndName(any(), any())).thenReturn(
+                true);
+
+            // When & Then
+            assertThatThrownBy(() -> categoryService.saveSubCategory(registerSubCategoryCommand))
+                .isInstanceOf(DuplicateCategoryNameException.class);
+            verify(subCategoryRepository, times(1)).existsByMainCategoryAndName(any(), any());
+            verify(mainCategoryRepository, times(1)).findById(anyLong());
         }
     }
 }
