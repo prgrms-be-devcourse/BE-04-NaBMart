c0274c39b42601e61dce54a2aaf15a0e277d7e2a
Seongju <lsj40413@gmail.com>
2023-09-04 16:23:38 +0900

    [NAYB-47] refactor : RegisterSubCategory request 필드 수정

--- src/main/java/com/prgrms/nabmart/domain/category/controller/request/RegisterSubCategoryRequest.java
+++ src/main/java/com/prgrms/nabmart/domain/category/controller/request/RegisterSubCategoryRequest.java
@@ -1,8 +1,11 @@
 package com.prgrms.nabmart.domain.category.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record RegisterSubCategoryRequest(
+    @NotNull(message = "대카테고리는 필수 항목입니다.") @Positive(message = "없는 대카테고리 Id입니다.") Long mainCategoryId,
     @NotBlank(message = "소카테고리명은 필수 항목입니다.") String name
 ) {
 
--- src/main/java/com/prgrms/nabmart/domain/category/service/request/RegisterSubCategoryCommand.java
+++ src/main/java/com/prgrms/nabmart/domain/category/service/request/RegisterSubCategoryCommand.java
@@ -2,13 +2,20 @@ package com.prgrms.nabmart.domain.category.service.request;
 
 import com.prgrms.nabmart.domain.category.controller.request.RegisterSubCategoryRequest;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record RegisterSubCategoryCommand(
+    @NotNull(message = "대카테고리는 필수 항목입니다.") @Positive(message = "없는 대카테고리 Id입니다.") Long mainCategoryId,
     @NotBlank(message = "소카테고리명은 필수 항목입니다.") String name
 ) {
 
     public static RegisterSubCategoryCommand from(
-        RegisterSubCategoryRequest registerSubCategoryRequest) {
-        return new RegisterSubCategoryCommand(registerSubCategoryRequest.name());
+        RegisterSubCategoryRequest registerSubCategoryRequest
+    ) {
+        return new RegisterSubCategoryCommand(
+            registerSubCategoryRequest.mainCategoryId(),
+            registerSubCategoryRequest.name()
+        );
     }
 }
