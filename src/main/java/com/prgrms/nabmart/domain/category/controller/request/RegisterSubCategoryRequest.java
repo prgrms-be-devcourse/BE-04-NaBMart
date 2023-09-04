579714816d26e1e00f530188f5fc05f22e793510
Seongju <lsj40413@gmail.com>
2023-09-04 17:46:50 +0900

    [NAYB-47] refactor : Validate 필드 메시지 수정

--- src/main/java/com/prgrms/nabmart/domain/category/controller/request/RegisterSubCategoryRequest.java
+++ src/main/java/com/prgrms/nabmart/domain/category/controller/request/RegisterSubCategoryRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record RegisterSubCategoryRequest(
-    @NotNull(message = "대카테고리는 필수 항목입니다.") @Positive(message = "없는 대카테고리 Id입니다.") Long mainCategoryId,
+    @NotNull(message = "대카테고리는 필수 항목입니다.") @Positive(message = "대카테고리 Id는 1 이상이어야 합니다.") Long mainCategoryId,
     @NotBlank(message = "소카테고리명은 필수 항목입니다.") String name
 ) {
 
