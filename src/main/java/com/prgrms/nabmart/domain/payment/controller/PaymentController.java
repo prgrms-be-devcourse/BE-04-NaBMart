d317d88ea6a3fa64ca911f889872d8af6342f403
pushedrumex <pushedrumex@naver.com>
2023-09-04 23:14:06 +0900

    [NAYB-28]feat: PaymentController.pay 핸들러 생성

---
+++ src/main/java/com/prgrms/nabmart/domain/payment/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.prgrms.nabmart.domain.payment.controller;
+
+import com.prgrms.nabmart.domain.payment.controller.request.PaymentRequest;
+import com.prgrms.nabmart.domain.payment.controller.response.PaymentResponse;
+import com.prgrms.nabmart.domain.payment.service.PaymentService;
+import com.prgrms.nabmart.domain.payment.service.request.PaymentCommand;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/pays")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/{orderId}")
+    public ResponseEntity<PaymentResponse> pay(
+            @RequestBody @Valid PaymentRequest paymentRequest,
+            @PathVariable Long orderId
+    ) {
+        PaymentCommand paymentCommand = PaymentCommand.from(paymentRequest);
+        return ResponseEntity.ok(paymentService.pay(orderId, paymentCommand));
+    }
+}
