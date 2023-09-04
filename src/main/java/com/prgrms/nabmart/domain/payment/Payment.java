6943555f88ad7ea9cf080db73cd03190b0a36adf
pushedrumex <pushedrumex@naver.com>
2023-09-04 18:43:51 +0900

    [NAYB-28]feat: payment Entity 생성

---
+++ src/main/java/com/prgrms/nabmart/domain/payment/Payment.java
@@ -0,0 +1,39 @@
+package com.prgrms.nabmart.domain.payment;
+
+import com.prgrms.nabmart.domain.BaseTimeEntity;
+import com.prgrms.nabmart.domain.order.Order;
+import com.prgrms.nabmart.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "payment")
+public class Payment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long payId;
+
+    @Setter
+    private String paymentKey;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private PaymentType paymentType;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
