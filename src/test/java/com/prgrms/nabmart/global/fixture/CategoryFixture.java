8c99095104895d21de11d6ab62e78b57ada5f632
Seongju <lsj40413@gmail.com>
2023-09-04 16:55:12 +0900

    [NAYB-47] refactor : CategoryFixture

--- src/test/java/com/prgrms/nabmart/global/fixture/CategoryFixture.java
+++ src/test/java/com/prgrms/nabmart/global/fixture/CategoryFixture.java
@@ -2,6 +2,10 @@ package com.prgrms.nabmart.global.fixture;
 
 import com.prgrms.nabmart.domain.category.MainCategory;
 import com.prgrms.nabmart.domain.category.SubCategory;
+import com.prgrms.nabmart.domain.category.controller.request.RegisterMainCategoryRequest;
+import com.prgrms.nabmart.domain.category.controller.request.RegisterSubCategoryRequest;
+import com.prgrms.nabmart.domain.category.service.request.RegisterMainCategoryCommand;
+import com.prgrms.nabmart.domain.category.service.request.RegisterSubCategoryCommand;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +14,7 @@ public final class CategoryFixture {
 
     private static final String MAIN_CATEGORY_NAME = "대카테고리";
     private static final String SUB_CATEGORY_NAME = "소카테고리";
+    private static final Long MAIN_CATEGORY_ID = 1L;
 
     public static MainCategory mainCategory() {
         return new MainCategory(MAIN_CATEGORY_NAME);
