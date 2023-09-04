184a11cf387783dc5888498b7093678729aec28d
Seongju <lsj40413@gmail.com>
2023-09-04 16:27:11 +0900

    [NAYB-47] feat : 소카테고리 저장 로직

--- src/main/java/com/prgrms/nabmart/domain/category/service/CategoryService.java
+++ src/main/java/com/prgrms/nabmart/domain/category/service/CategoryService.java
@@ -1,9 +1,13 @@
 package com.prgrms.nabmart.domain.category.service;
 
 import com.prgrms.nabmart.domain.category.MainCategory;
+import com.prgrms.nabmart.domain.category.SubCategory;
 import com.prgrms.nabmart.domain.category.exception.DuplicateCategoryNameException;
+import com.prgrms.nabmart.domain.category.exception.NoSuchCategoryException;
 import com.prgrms.nabmart.domain.category.repository.MainCategoryRepository;
+import com.prgrms.nabmart.domain.category.repository.SubCategoryRepository;
 import com.prgrms.nabmart.domain.category.service.request.RegisterMainCategoryCommand;
+import com.prgrms.nabmart.domain.category.service.request.RegisterSubCategoryCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class CategoryService {
 
     private final MainCategoryRepository mainCategoryRepository;
+    private final SubCategoryRepository subCategoryRepository;
 
     @Transactional
     public Long saveMainCategory(RegisterMainCategoryCommand registerMainCategoryCommand) {
@@ -21,7 +26,20 @@ public class CategoryService {
             throw new DuplicateCategoryNameException("이미 존재하는 카테고리입니다.");
         }
         MainCategory mainCategory = new MainCategory(newCategoryName);
-        MainCategory savedMainCategory = mainCategoryRepository.save(mainCategory);
-        return savedMainCategory.getMainCategoryId();
+        return mainCategoryRepository.save(mainCategory).getMainCategoryId();
+    }
+
+    @Transactional
+    public Long saveSubCategory(RegisterSubCategoryCommand registerSubCategoryCommand) {
+        String newSubCategoryName = registerSubCategoryCommand.name();
+        Long parentCategoryId = registerSubCategoryCommand.mainCategoryId();
+        MainCategory mainCategory = mainCategoryRepository.findById(parentCategoryId)
+            .orElseThrow(() -> new NoSuchCategoryException("없는 대카테고리입니다."));
+
+        if (subCategoryRepository.existsByMainCategoryAndName(mainCategory, newSubCategoryName)) {
+            throw new DuplicateCategoryNameException("이미 존재하는 소카테고리입니다.");
+        }
+        SubCategory subCategory = new SubCategory(mainCategory, newSubCategoryName);
+        return subCategoryRepository.save(subCategory).getSubCategoryId();
     }
 }
