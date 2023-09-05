40ef0864feac4c54836c2769ff84649501f4c34b
seongHyun-Min <steve6133@naver.com>
2023-09-05 16:27:16 +0900

    [NAYB-35] feat : UserCoupon 도메인 생성

---
+++ src/main/java/com/prgrms/nabmart/domain/coupon/UserCoupon.java
@@ -0,0 +1,43 @@
+package com.prgrms.nabmart.domain.coupon;
+
+import com.prgrms.nabmart.domain.user.User;
+import com.prgrms.nabmart.global.BaseTimeEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserCoupon extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userCouponId;
+
+    @Column(nullable = false)
+    private boolean isUsed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "coupon_id")
+    private Coupon coupon;
+
+    public UserCoupon(User user, Coupon coupon) {
+        this.user = user;
+        this.coupon = coupon;
+        isUsed = true;
+    }
+}
