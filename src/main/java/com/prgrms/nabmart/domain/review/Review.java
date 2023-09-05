40b4c0d01779d865406c54040c04acd71d1ce470
baejunil <bjo6300@naver.com>
2023-09-05 23:39:01 +0900

    [NAYB-29] feat : 리뷰 도메인 검증 구현

--- src/main/java/com/prgrms/nabmart/domain/review/Review.java
+++ src/main/java/com/prgrms/nabmart/domain/review/Review.java
@@ -1,7 +1,12 @@
 package com.prgrms.nabmart.domain.review;
 
+import static java.util.Objects.isNull;
+
+import com.prgrms.nabmart.domain.cart.exception.InvalidCartItemException;
 import com.prgrms.nabmart.domain.item.domain.Item;
+import com.prgrms.nabmart.domain.review.exception.InvalidReviewException;
 import com.prgrms.nabmart.domain.user.User;
+import com.prgrms.nabmart.domain.user.exception.NotFoundUserException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -42,9 +47,50 @@ public class Review {
 
     @Builder
     public Review(User user, Item item, double rate, String content) {
+        validateUser(user);
+        validateItem(item);
+        validateRate(rate);
+        validateContent(content);
+
         this.user = user;
         this.item = item;
         this.rate = rate;
         this.content = content;
+    }
+
+    public void validateUser(
+        final User user
+    ) {
+        if (isNull(user)) {
+            throw new NotFoundUserException("User 가 존재하지 않습니다.");
+        }
+    }
+
+    public void validateItem(
+        final Item item
+    ) {
+        if (isNull(item)) {
+            throw new InvalidCartItemException("Item 이 존재하지 않습니다.");
+        }
+    }
+
+    public void validateRate(
+        double rate
+    ) {
+        if (0 < rate) {
+            throw new InvalidReviewException("평점은 양수여야 합니다.");
+        }
+
+        if (rate > 5) {
+            rate = 5;
+        }
+    }
+
+    public void validateContent(
+        final String content
+    ) {
+        if (content.length() > 100) {
+            throw new InvalidReviewException("리뷰 내용은 100자를 넘을 수 없습니다.");
+        }
     }
 }
