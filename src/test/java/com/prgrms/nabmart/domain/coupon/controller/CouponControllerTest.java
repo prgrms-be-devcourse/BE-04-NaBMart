1df04cc0744aa7cb32bc8b61d0cb7b2171dc4969
seongHyun-Min <steve6133@naver.com>
2023-09-06 00:41:59 +0900

    [NAYB-35] fix : application.yml mode : alawys 추가

--- src/test/java/com/prgrms/nabmart/domain/coupon/controller/CouponControllerTest.java
+++ src/test/java/com/prgrms/nabmart/domain/coupon/controller/CouponControllerTest.java
@@ -17,7 +17,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prgrms.nabmart.domain.coupon.service.CouponService;
 import com.prgrms.nabmart.domain.coupon.service.request.RegisterCouponCommand;
 import com.prgrms.nabmart.domain.coupon.service.request.RegisterUserCouponCommand;
+import com.prgrms.nabmart.global.auth.support.AuthFixture;
 import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -27,6 +29,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -43,6 +47,12 @@ class CouponControllerTest {
 
     @MockBean
     private CouponService couponService;
+
+    @BeforeEach
+    void setUp() {
+        Authentication authentication = AuthFixture.usernamePasswordAuthenticationToken();
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
 
     @Nested
     @DisplayName("쿠폰 생성하는 api 호출 시")
