a16aa8c3d7d713738795cec6f21c52071c453c83
hseong3243 <hseong3243@gmail.com>
2023-09-05 19:54:14 +0900

    [NAYB-93] refactor: `User`를 찾지 못하는 예외를 `UserNotFoundException`으로 통일

--- src/main/java/com/prgrms/nabmart/domain/user/exception/NotExistUserException.java
+++
@@ -1,8 +0,0 @@
-package com.prgrms.nabmart.domain.user.exception;
-
-public class NotExistUserException extends UserException {
-
-    public NotExistUserException(final String message) {
-        super(message);
-    }
-}
