e85ee8998e83cadce53d49733a5c5cf10aea03a7
seongHyun-Min <steve6133@naver.com>
2023-09-05 16:28:48 +0900

    [NAYB-35] feat : NotFoundCouponException 생성

---
+++ src/main/java/com/prgrms/nabmart/domain/coupon/exception/NotFoundCouponException.java
@@ -0,0 +1,8 @@
+package com.prgrms.nabmart.domain.coupon.exception;
+
+public class NotFoundCouponException extends CouponException {
+
+    public NotFoundCouponException(String message) {
+        super(message);
+    }
+}
