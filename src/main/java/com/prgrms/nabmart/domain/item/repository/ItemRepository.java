7a1abb98cf0f3cfcb491d7b1d8cd194d85e2e239
Seongju <lsj40413@gmail.com>
2023-09-07 00:09:13 +0900

    [NAYB-102] feat : 대카테고리 전체 조회 - 할인율 높은 순 쿼리 메서드 선언

--- src/main/java/com/prgrms/nabmart/domain/item/repository/ItemRepository.java
+++ src/main/java/com/prgrms/nabmart/domain/item/repository/ItemRepository.java
@@ -7,13 +7,17 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @Query("select i from Item i where i.itemId in ?1")
     List<Item> findByItemIdIn(Collection<Long> itemIds);
 
-    List<Item> findByItemIdLessThanAndMainCategoryOrderByItemIdDesc(@Param("itemId") Long itemId,
-        @Param("mainCategory") MainCategory mainCategory, Pageable pageable);
+    // 대카테고리 전체 조회 - 최신 등록 순
+    List<Item> findByItemIdLessThanAndMainCategoryOrderByItemIdDesc(Long itemId,
+        MainCategory mainCategory, Pageable pageable);
+
+    // 대카테고리 전체 조회 - 할인율 높은 순
+    List<Item> findByDiscountLessThanAndMainCategoryOrderByDiscountDescItemIdDesc(int discount,
+        MainCategory mainCategory, Pageable pageable);
 }
