545b93a4c6d2325200e2a28cc637c024285d68ba
baejunil <bjo6300@naver.com>
2023-09-06 21:50:52 +0900

    [NAYB-30] test : review 수정 controller 테스트 구현

---
+++ src/test/java/com/prgrms/nabmart/domain/review/controller/request/UpdateReviewRequestTest.java
@@ -0,0 +1,118 @@
+package com.prgrms.nabmart.domain.review.controller.request;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.prgrms.nabmart.domain.review.support.UpdateReviewRequestFixture;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class UpdateReviewRequestTest {
+
+    private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    @Nested
+    @DisplayName("UpdateReviewRequest 생성 시")
+    class ValidateUpdateReviewRequest {
+
+        @Test
+        @DisplayName("성공")
+        void success() {
+            // given
+            UpdateReviewRequest registerReviewRequest = UpdateReviewRequestFixture.updateReviewRequest(
+                5, "내공 냠냠"
+            );
+
+            // when
+            Set<ConstraintViolation<UpdateReviewRequest>> violations = validator.validate(
+                registerReviewRequest);
+
+            // then
+            assertThat(violations).hasSize(0);
+        }
+
+        @Test
+        @DisplayName("예외 : rate 가 0미만인 경우")
+        void throwExceptionWhenRateIsNull() {
+            // given
+            UpdateReviewRequest registerReviewRequest = UpdateReviewRequestFixture.updateReviewRequest(
+                -1, "내공 냠냠"
+            );
+
+            // when
+            Set<ConstraintViolation<UpdateReviewRequest>> violations = validator.validate(
+                registerReviewRequest);
+
+            // then
+            assertThat(violations).hasSize(1);
+        }
+
+        @Test
+        @DisplayName("예외 : rate 가 5점 초과")
+        void throwExceptionWhenRateIsMoreThanMaxLength() {
+            // given
+            UpdateReviewRequest registerReviewRequest = UpdateReviewRequestFixture.updateReviewRequest(
+                6, "내공 냠냠"
+            );
+
+            // when
+            Set<ConstraintViolation<UpdateReviewRequest>> violations = validator.validate(
+                registerReviewRequest);
+
+            // then
+            assertThat(violations).hasSize(1);
+        }
+
+        @Test
+        @DisplayName("예외 : content 가 null")
+        void throwExceptionWhenContentIsNull() {
+            // given
+            UpdateReviewRequest registerReviewRequest = UpdateReviewRequestFixture.updateReviewRequest(
+                5, null
+            );
+
+            // when
+            Set<ConstraintViolation<UpdateReviewRequest>> violations = validator.validate(
+                registerReviewRequest);
+
+            // then
+            assertThat(violations).hasSize(1);
+        }
+
+        @Test
+        @DisplayName("예외 : content 가 빈 칸인 경우")
+        void throwExceptionWhenContentIsBlank() {
+            // given
+            UpdateReviewRequest registerReviewRequest = UpdateReviewRequestFixture.updateReviewRequest(
+                5, ""
+            );
+
+            // when
+            Set<ConstraintViolation<UpdateReviewRequest>> violations = validator.validate(
+                registerReviewRequest);
+
+            // then
+            assertThat(violations).hasSize(2);
+        }
+
+        @Test
+        @DisplayName("예외 : content 가 100자 초과인 경우")
+        void throwExceptionWhenContentIsMoreThanMaxLength() {
+            // given
+            UpdateReviewRequest registerReviewRequest = UpdateReviewRequestFixture.updateReviewRequest(
+                5, "z".repeat(101)
+            );
+
+            // when
+            Set<ConstraintViolation<UpdateReviewRequest>> violations = validator.validate(
+                registerReviewRequest);
+
+            // then
+            assertThat(violations).hasSize(1);
+        }
+    }
+}
