cc41d48310983cc88c30bc864602d97a42bbe30d
hseong3243 <hseong3243@gmail.com>
2023-09-06 16:56:33 +0900

    [NAYB-20] feat: 모든 `User`의 직전달 주문횟수에 따른 등급 업데이트 추가

---
+++ src/main/java/com/prgrms/nabmart/domain/user/repository/UserOrderCount.java
@@ -0,0 +1,6 @@
+package com.prgrms.nabmart.domain.user.repository;
+
+public interface UserOrderCount {
+    Long getUserId();
+    Integer getOrderCount();
+}
--- src/main/java/com/prgrms/nabmart/domain/user/repository/UserRepository.java
+++ src/main/java/com/prgrms/nabmart/domain/user/repository/UserRepository.java
@@ -1,10 +1,33 @@
 package com.prgrms.nabmart.domain.user.repository;
 
 import com.prgrms.nabmart.domain.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.prgrms.nabmart.domain.user.UserGrade;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByProviderAndProviderId(String provider, String providerId);
+
+    @Query("select o.user.userId as userId, count(o) as orderCount from Order o"
+        + " where o.createdAt between :start and :end"
+        + " group by o.user.userId"
+        + " order by o.user.userId asc")
+    List<UserOrderCount> getUserOrderCount(
+        @Param("start") LocalDateTime start,
+        @Param("end") LocalDateTime end,
+        Pageable pageable);
+
+    @Modifying
+    @Query("update User u set u.userGrade = :userGrade"
+        + " where u.userId in :userIds")
+    void updateUserGrade(
+        @Param("userGrade")UserGrade userGrade,
+        @Param("userIds") List<Long> userIds);
 }
