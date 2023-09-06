328e9a42ec91cbd1c1c6da7a2977f6174724f1a8
hseong3243 <hseong3243@gmail.com>
2023-09-06 22:12:42 +0900

    [NAYB-19] refactor: `RegisterLikeItemCommand` 정적 팩토리 메서드 파라미터에 `final` 추가

--- src/main/java/com/prgrms/nabmart/domain/item/service/request/RegisterLikeItemCommand.java
+++ src/main/java/com/prgrms/nabmart/domain/item/service/request/RegisterLikeItemCommand.java
@@ -2,7 +2,7 @@ package com.prgrms.nabmart.domain.item.service.request;
 
 public record RegisterLikeItemCommand(Long userId, Long itemId) {
 
-    public static RegisterLikeItemCommand of(Long userId, Long itemId) {
+    public static RegisterLikeItemCommand of(final Long userId, final Long itemId) {
         return new RegisterLikeItemCommand(userId, itemId);
     }
 }
