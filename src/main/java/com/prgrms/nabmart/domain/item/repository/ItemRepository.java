69c6abcc82c0f87c507f7f0c64c518c6f09e1e6d
Seongju <lsj40413@gmail.com>
2023-09-07 18:20:00 +0900

    [NAYB-102] feat : 정렬 기준별 대카테고리 아이템 목록 조회 Repository Layer

--- src/main/java/com/prgrms/nabmart/domain/item/repository/ItemRepository.java
+++ src/main/java/com/prgrms/nabmart/domain/item/repository/ItemRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
@@ -20,4 +21,25 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     // 대카테고리 전체 조회 - 할인율 높은 순
     List<Item> findByDiscountLessThanAndMainCategoryOrderByDiscountDescItemIdDesc(int discount,
         MainCategory mainCategory, Pageable pageable);
+
+    // 대카테고리 전체 조회 - 금액 높은 순
+    List<Item> findByPriceLessThanAndMainCategoryOrderByPriceDescItemIdDesc(int price,
+        MainCategory mainCategory, Pageable pageable);
+
+    // 대카테고리 전체 조회 - 금액 낮은 순
+    List<Item> findByPriceGreaterThanAndMainCategoryOrderByPriceAscItemIdDesc(int price,
+        MainCategory mainCategory, Pageable pageable);
+
+    // 대카테고리 전체 조회 - 주문 많은 순
+    @Query("SELECT i "
+        + "FROM Item i "
+        + "LEFT JOIN OrderItem oi ON oi.item = i "
+        + "WHERE i.mainCategory = :mainCategory "
+        + "GROUP BY i "
+        + "HAVING SUM(oi.quantity) < :totalOrderedQuantity "
+        + "ORDER BY SUM(oi.quantity) DESC")
+    List<Item> findByOrderCountAndMainCategory(
+        @Param("totalOrderedQuantity") int totalOrderedQuantity,
+        @Param("mainCategory") MainCategory mainCategory,
+        Pageable pageable);
 }
