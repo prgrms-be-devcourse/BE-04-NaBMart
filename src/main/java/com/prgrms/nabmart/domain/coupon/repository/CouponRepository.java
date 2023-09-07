74991617b1be1db5ff7f2cbe10a0aef368c95899
seongHyun-Min <steve6133@naver.com>
2023-09-07 10:20:44 +0900

    [NAYB-51] feat : CouponRepository findByEndAtGreaterThan 구현

--- src/main/java/com/prgrms/nabmart/domain/coupon/repository/CouponRepository.java
+++ src/main/java/com/prgrms/nabmart/domain/coupon/repository/CouponRepository.java
@@ -1,8 +1,13 @@
 package com.prgrms.nabmart.domain.coupon.repository;
 
 import com.prgrms.nabmart.domain.coupon.Coupon;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
+    List<Coupon> findByEndAtGreaterThan(LocalDate currentDate);
+
 }
+
