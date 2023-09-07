8d92aed04bc367a6f6676e5a1be0b2e86224c593
Seongju <lsj40413@gmail.com>
2023-09-07 23:36:14 +0900

    [NAYB-102] feat : 각 정렬기준별 대카테고리 품목 조회 서비스 로직

--- src/main/java/com/prgrms/nabmart/domain/item/service/ItemService.java
+++ src/main/java/com/prgrms/nabmart/domain/item/service/ItemService.java
@@ -7,22 +7,25 @@ import com.prgrms.nabmart.domain.item.Item;
 import com.prgrms.nabmart.domain.item.ItemSortType;
 import com.prgrms.nabmart.domain.item.exception.NotFoundItemException;
 import com.prgrms.nabmart.domain.item.repository.ItemRepository;
-import com.prgrms.nabmart.domain.item.service.request.FindItemsByMainCategoryCommand;
 import com.prgrms.nabmart.domain.item.service.request.FindItemDetailCommand;
+import com.prgrms.nabmart.domain.item.service.request.FindItemsByMainCategoryCommand;
 import com.prgrms.nabmart.domain.item.service.response.FindItemDetailResponse;
 import com.prgrms.nabmart.domain.item.service.response.FindItemsResponse;
-import java.util.ArrayList;
+import com.prgrms.nabmart.domain.order.repository.OrderItemRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ItemService {
 
     private final ItemRepository itemRepository;
+    private final OrderItemRepository orderItemRepository;
     private final MainCategoryRepository mainCategoryRepository;
 
 
@@ -30,32 +33,8 @@ public class ItemService {
     public FindItemsResponse findItemsByMainCategory(
         FindItemsByMainCategoryCommand findItemsByMainCategoryCommand) {
 
-        List<Item> items = findItemsByMainCategoryFrom(
-            findItemsByMainCategoryCommand);
+        List<Item> items = findItemsByMainCategoryFrom(findItemsByMainCategoryCommand);
         return FindItemsResponse.from(items);
-    }
-
-
-    private List<Item> findItemsByMainCategoryFrom(
-        FindItemsByMainCategoryCommand findItemsByMainCategoryCommand) {
-
-        Long previousItemId = findItemsByMainCategoryCommand.previousItemId();
-        ItemSortType itemSortType = findItemsByMainCategoryCommand.itemSortType();
-        PageRequest pageRequest = findItemsByMainCategoryCommand.pageRequest();
-        String mainCategoryName = findItemsByMainCategoryCommand.mainCategoryName().toLowerCase();
-        MainCategory mainCategory = mainCategoryRepository.findByName(mainCategoryName)
-            .orElseThrow(() -> new NotFoundCategoryException("없는 대카테고리입니다."));
-
-        switch (itemSortType) {
-            case NEW -> {
-                return itemRepository.findByItemIdLessThanAndMainCategoryOrderByItemIdDesc(
-                    previousItemId, mainCategory, pageRequest);
-            }
-
-            default -> {
-                return new ArrayList<>();
-            }
-        }
     }
 
     @Transactional(readOnly = true)
@@ -75,5 +54,43 @@ public class ItemService {
             item.getLikeItems().size(),
             item.getMaxBuyQuantity()
         );
+    }
+
+    private List<Item> findItemsByMainCategoryFrom(
+        FindItemsByMainCategoryCommand findItemsByMainCategoryCommand) {
+
+        Long lastIdx = findItemsByMainCategoryCommand.lastIdx();
+        ItemSortType itemSortType = findItemsByMainCategoryCommand.itemSortType();
+        PageRequest pageRequest = findItemsByMainCategoryCommand.pageRequest();
+        String mainCategoryName = findItemsByMainCategoryCommand.mainCategoryName().toLowerCase();
+        MainCategory mainCategory = mainCategoryRepository.findByName(mainCategoryName)
+            .orElseThrow(() -> new NotFoundCategoryException("없는 대카테고리입니다."));
+
+        switch (itemSortType) {
+            case NEW -> {
+                return itemRepository.findByItemIdLessThanAndMainCategoryOrderByItemIdDesc(
+                    lastIdx, mainCategory, pageRequest);
+            }
+            case HIGHEST_AMOUNT -> {
+                int price = lastIdx.intValue();
+                return itemRepository.findByPriceLessThanAndMainCategoryOrderByPriceDescItemIdDesc(
+                    price, mainCategory, pageRequest);
+            }
+            case LOWEST_AMOUNT -> {
+                int price = lastIdx.intValue();
+                return itemRepository.findByPriceGreaterThanAndMainCategoryOrderByPriceAscItemIdDesc(
+                    price, mainCategory, pageRequest);
+            }
+            case DISCOUNT -> {
+                int discountRate = lastIdx.intValue();
+                return itemRepository.findByDiscountLessThanAndMainCategoryOrderByDiscountDescItemIdDesc(
+                    discountRate, mainCategory, pageRequest);
+            }
+            default -> {
+                Long totalOrderedQuantity = orderItemRepository.countByOrderItemId(lastIdx);
+                return itemRepository.findByOrderedQuantityAndMainCategory(
+                    totalOrderedQuantity, mainCategory, pageRequest);
+            }
+        }
     }
 }
