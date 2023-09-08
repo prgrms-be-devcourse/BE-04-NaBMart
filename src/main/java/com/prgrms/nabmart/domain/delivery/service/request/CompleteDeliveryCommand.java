0b8ecf47fe864824c8c58de94a3a1da2380cac49
hseong3243 <hseong3243@gmail.com>
2023-09-08 23:27:46 +0900

    [NAYB-112] feat: 배달완료 상태 변경 API 추가

--- src/main/java/com/prgrms/nabmart/domain/delivery/service/request/CompleteDeliveryCommand.java
+++ src/main/java/com/prgrms/nabmart/domain/delivery/service/request/CompleteDeliveryCommand.java
@@ -1,4 +1,8 @@
 package com.prgrms.nabmart.domain.delivery.service.request;
 
 public record CompleteDeliveryCommand(Long deliveryId) {
+
+    public static CompleteDeliveryCommand of(final Long deliveryId) {
+        return new CompleteDeliveryCommand(deliveryId);
+    }
 }
