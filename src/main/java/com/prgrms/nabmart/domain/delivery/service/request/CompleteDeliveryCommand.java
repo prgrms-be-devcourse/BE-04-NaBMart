7d35e37e06b636aad17db9645ab39cef2eb1d898
hseong3243 <48748265+hseong3243@users.noreply.github.com>
2023-09-09 08:04:27 +0900

    [NAYB-112] refactor: dto 정적 팩토리 메서드 컨벤션에 따라 이름 변경
    Co-authored-by: 배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>

--- src/main/java/com/prgrms/nabmart/domain/delivery/service/request/CompleteDeliveryCommand.java
+++ src/main/java/com/prgrms/nabmart/domain/delivery/service/request/CompleteDeliveryCommand.java
@@ -2,7 +2,7 @@ package com.prgrms.nabmart.domain.delivery.service.request;
 
 public record CompleteDeliveryCommand(Long deliveryId) {
 
-    public static CompleteDeliveryCommand of(final Long deliveryId) {
+    public static CompleteDeliveryCommand from(final Long deliveryId) {
         return new CompleteDeliveryCommand(deliveryId);
     }
 }
