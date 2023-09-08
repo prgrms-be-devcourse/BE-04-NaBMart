d935d0ad996fb87e46505cfc1cf83ddffa599fc3
hseong3243 <48748265+hseong3243@users.noreply.github.com>
2023-09-09 08:12:58 +0900

    Revert "[NAYB-112] feat: 라이더가 배달을 완료하면 배달 현황은 '배달완료'로 변경된다."

--- src/main/java/com/prgrms/nabmart/domain/delivery/service/request/CompleteDeliveryCommand.java
+++
@@ -1,8 +0,0 @@
-package com.prgrms.nabmart.domain.delivery.service.request;
-
-public record CompleteDeliveryCommand(Long deliveryId) {
-
-    public static CompleteDeliveryCommand from(final Long deliveryId) {
-        return new CompleteDeliveryCommand(deliveryId);
-    }
-}
