ed7f451dd2d1d43f8ab04287ac95f0d81ae0b1ea
hseong3243 <hseong3243@gmail.com>
2023-09-10 20:29:13 +0900

    [NAYB-120] feat: `Rider` 엔티티 추가

---
+++ src/main/java/com/prgrms/nabmart/domain/delivery/Rider.java
@@ -0,0 +1,37 @@
+package com.prgrms.nabmart.domain.delivery;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Rider {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long riderId;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Builder
+    public Rider(String username, String password, String address) {
+        this.username = username;
+        this.password = password;
+        this.address = address;
+    }
+}
