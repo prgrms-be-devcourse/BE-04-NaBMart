45b326783b9e72a2d2fb1742bec394336b5bc236
hseong3243 <hseong3243@gmail.com>
2023-09-10 22:24:42 +0900

    [NAYB-120] feat: 라이더 회원가입 API 추가

--- src/main/java/com/prgrms/nabmart/domain/delivery/service/request/RiderSignupCommand.java
+++ src/main/java/com/prgrms/nabmart/domain/delivery/service/request/RiderSignupCommand.java
@@ -2,4 +2,10 @@ package com.prgrms.nabmart.domain.delivery.service.request;
 
 public record RiderSignupCommand(String username, String password, String address) {
 
+    public static RiderSignupCommand of(
+        final String username,
+        final String password,
+        final String address) {
+        return new RiderSignupCommand(username, password, address);
+    }
 }
