a7dbcf3fa0b6f06e310ba14f160f2f104429e47d
seongHyun-Min <steve6133@naver.com>
2023-09-11 00:12:29 +0900

    [NAYB-24] feat : OrderController 주문 초기값 생성 API 구현

--- src/main/java/com/prgrms/nabmart/domain/order/controller/OrderController.java
+++ src/main/java/com/prgrms/nabmart/domain/order/controller/OrderController.java
@@ -1,12 +1,18 @@
 package com.prgrms.nabmart.domain.order.controller;
 
+import com.prgrms.nabmart.domain.order.controller.request.CreateOrderRequest;
 import com.prgrms.nabmart.domain.order.service.OrderService;
+import com.prgrms.nabmart.domain.order.service.request.CreateOrdersCommand;
 import com.prgrms.nabmart.domain.order.service.response.FindOrderDetailResponse;
 import com.prgrms.nabmart.global.auth.LoginUser;
+import jakarta.validation.Valid;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final OrderService orderService;
+
+    @PostMapping()
+    public ResponseEntity<Void> createOrder(
+        @Valid @RequestBody final CreateOrderRequest createOrderRequest,
+        @LoginUser final Long userId
+    ) {
+        CreateOrdersCommand createOrdersCommand = CreateOrdersCommand.of(userId,
+            createOrderRequest);
+        Long orderId = orderService.createOrder(createOrdersCommand);
+        URI location = URI.create("/api/v1/orders" + orderId);
+        return ResponseEntity.created(location).build();
+    }
 
     @GetMapping("/{orderId}")
     public ResponseEntity<FindOrderDetailResponse> findOrderByIdAndUserId(
