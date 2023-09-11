12cd6868ad48c96bcc4e6b14a05e988416192e3a
seongHyun-Min <steve6133@naver.com>
2023-09-11 20:41:27 +0900

    [NAYB-123] feat : InvalidOrderException 예외 추가

---
+++ src/main/java/com/prgrms/nabmart/domain/order/exception/InvalidOrderException.java
@@ -0,0 +1,8 @@
+package com.prgrms.nabmart.domain.order.exception;
+
+public class InvalidOrderException extends OrderException {
+
+    public InvalidOrderException(final String message) {
+        super(message);
+    }
+}
