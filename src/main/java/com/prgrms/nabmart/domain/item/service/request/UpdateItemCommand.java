68f1acec7f78d4f45fc270020ce4e69d398c00de
Seongju <lsj40413@gmail.com>
2023-09-12 15:50:40 +0900

    [NAYB-10] feat : 도메인 업데이트 로직

---
+++ src/main/java/com/prgrms/nabmart/domain/item/service/request/UpdateItemCommand.java
@@ -0,0 +1,37 @@
+package com.prgrms.nabmart.domain.item.service.request;
+
+import com.prgrms.nabmart.domain.item.controller.request.UpdateItemRequest;
+import lombok.Builder;
+
+@Builder
+public record UpdateItemCommand(Long itemId, String name, int quantity, int price,
+                                String description, Long mainCategoryId, Long subCategoryId,
+                                int discount) {
+
+    public static UpdateItemCommand of(
+        final Long itemId,
+        final String name,
+        final int quantity,
+        final int price,
+        final String description,
+        final Long mainCategoryId,
+        final Long subCategoryId,
+        final int discount
+    ) {
+        return new UpdateItemCommand(itemId, name, quantity, price, description, mainCategoryId,
+            subCategoryId, discount);
+    }
+
+    public static UpdateItemCommand from(UpdateItemRequest updateItemRequest) {
+        return UpdateItemCommand.builder()
+            .itemId(updateItemRequest.itemId())
+            .name(updateItemRequest.name())
+            .quantity(updateItemRequest.quantity())
+            .price(updateItemRequest.price())
+            .description(updateItemRequest.description())
+            .mainCategoryId(updateItemRequest.mainCategoryId())
+            .subCategoryId(updateItemRequest.subCategoryId())
+            .discount(updateItemRequest.discount())
+            .build();
+    }
+}
