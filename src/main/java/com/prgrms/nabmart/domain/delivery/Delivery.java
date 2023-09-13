e1f427b106cd9052d74fffafcf6b0467daf81a69
hseong3243 <hseong3243@gmail.com>
2023-09-13 16:59:06 +0900

    [NAYB-133] feat: `Delivery` 엔티티 연관관계 로딩 모두 지연로딩으로 변경

--- src/main/java/com/prgrms/nabmart/domain/delivery/Delivery.java
+++ src/main/java/com/prgrms/nabmart/domain/delivery/Delivery.java
@@ -7,6 +7,7 @@ import com.prgrms.nabmart.domain.user.User;
 import com.prgrms.nabmart.global.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -32,11 +33,11 @@ public class Delivery extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long deliveryId;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "orderId", nullable = false)
     private Order order;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "riderId")
     private Rider rider;
 
