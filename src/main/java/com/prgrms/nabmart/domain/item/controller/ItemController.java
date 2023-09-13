92c81668c543a78a4f8c724047632e89c773316a
Seongju <lsj40413@gmail.com>
2023-09-13 16:54:27 +0900

    [NAYB-7] feat: 상품등록 api

--- src/main/java/com/prgrms/nabmart/domain/item/controller/ItemController.java
+++ src/main/java/com/prgrms/nabmart/domain/item/controller/ItemController.java
@@ -1,21 +1,25 @@
 package com.prgrms.nabmart.domain.item.controller;
 
+import com.prgrms.nabmart.domain.item.controller.request.RegisterItemRequest;
 import com.prgrms.nabmart.domain.item.controller.request.UpdateItemRequest;
 import com.prgrms.nabmart.domain.item.service.ItemService;
 import com.prgrms.nabmart.domain.item.service.request.FindHotItemsCommand;
 import com.prgrms.nabmart.domain.item.service.request.FindItemDetailCommand;
 import com.prgrms.nabmart.domain.item.service.request.FindItemsByCategoryCommand;
 import com.prgrms.nabmart.domain.item.service.request.FindNewItemsCommand;
+import com.prgrms.nabmart.domain.item.service.request.RegisterItemCommand;
 import com.prgrms.nabmart.domain.item.service.request.UpdateItemCommand;
 import com.prgrms.nabmart.domain.item.service.response.FindItemDetailResponse;
 import com.prgrms.nabmart.domain.item.service.response.FindItemsResponse;
 import jakarta.validation.Valid;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,6 +33,7 @@ public class ItemController {
 
     private final ItemService itemService;
     private final String DEFAULT_PREVIOUS_ID = "-1";
+    private static final String BASE_URI = "/api/v1/items/";
 
     @GetMapping
     public ResponseEntity<FindItemsResponse> findItemsByCategory(
@@ -80,5 +85,15 @@ public class ItemController {
         UpdateItemCommand updateItemCommand = UpdateItemCommand.of(itemId, updateItemRequest);
         itemService.updateItem(updateItemCommand);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveItem(
+        @RequestBody @Valid RegisterItemRequest registerItemRequest
+    ) {
+        RegisterItemCommand registerItemCommand = RegisterItemCommand.from(registerItemRequest);
+        Long savedItemId = itemService.saveItem(registerItemCommand);
+        URI location = URI.create(BASE_URI + savedItemId);
+        return ResponseEntity.created(location).build();
     }
 }
---
+++ src/main/java/com/prgrms/nabmart/domain/item/controller/request/RegisterItemRequest.java
@@ -0,0 +1,36 @@
+package com.prgrms.nabmart.domain.item.controller.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Builder;
+
+@Builder
+public record RegisterItemRequest(
+
+    @NotBlank(message = "상품명은 필수 항목입니다.")
+    String name,
+
+    @PositiveOrZero(message = "상품 가격은 0원 이상이어야 합니다.")
+    @NotNull(message = "상품 가격은 필수 항목입니다.")
+    Integer price,
+
+    @PositiveOrZero(message = "상품 수량은 0개 이상이어야 합니다.")
+    @NotNull(message = "상품 수량은 필수 항목입니다.")
+    Integer quantity,
+
+    @Min(value = 0, message = "상품 할인율은 0% 이상이어야 합니다.")
+    @Max(value = 100, message = "상품 할인율은 100% 이하어야 합니다.")
+    int discount,
+
+    @NotNull(message = "최대 주문 수량은 필수 항목입니다.")
+    Integer maxBuyQuantity,
+    
+    String description,
+    Long mainCategoryId,
+    Long subCategoryId
+) {
+
+}
