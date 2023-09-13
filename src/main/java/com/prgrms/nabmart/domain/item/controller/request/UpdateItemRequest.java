0dc52853960e8add693c9295c2c8e2fe3bb50ccd
Seongju <lsj40413@gmail.com>
2023-09-13 16:54:48 +0900

    [NAYB-7] refactor: 상품수정 request 필드 수정
    maxBuyQuantity 추가

--- src/main/java/com/prgrms/nabmart/domain/item/controller/request/UpdateItemRequest.java
+++ src/main/java/com/prgrms/nabmart/domain/item/controller/request/UpdateItemRequest.java
@@ -3,6 +3,7 @@ package com.prgrms.nabmart.domain.item.controller.request;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 
 public record UpdateItemRequest(
@@ -11,30 +12,23 @@ public record UpdateItemRequest(
     String name,
 
     @PositiveOrZero(message = "상품 가격은 0원 이상이어야 합니다.")
-    int price,
+    @NotNull(message = "상품 가격은 필수 항목입니다.")
+    Integer price,
 
     @PositiveOrZero(message = "상품 수량은 0개 이상이어야 합니다.")
-    int quantity,
+    @NotNull(message = "상품 수량은 필수 항목입니다.")
+    Integer quantity,
 
     @Min(value = 0, message = "상품 할인율은 0% 이상이어야 합니다.")
     @Max(value = 100, message = "상품 할인율은 100% 이하어야 합니다.")
     int discount,
+
+    @NotNull(message = "최대 주문 수량은 필수 항목입니다.")
+    Integer maxBuyQuantity,
 
     String description,
     Long mainCategoryId,
     Long subCategoryId
 ) {
 
-    public static UpdateItemRequest of(
-        final String name,
-        final int price,
-        final int quantity,
-        final String description,
-        final Long mainCategoryId,
-        final Long subCategoryId,
-        final int discount
-    ) {
-        return new UpdateItemRequest(name, price, quantity, discount, description,
-            mainCategoryId, subCategoryId);
-    }
 }
