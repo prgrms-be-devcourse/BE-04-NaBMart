398a724fdedf3fdfca60873ee9114b75b44b90bd
hseong3243 <hseong3243@gmail.com>
2023-09-15 22:17:09 +0900

    [NAYB-151] fix: `Delivery` enum 필드에 @Enumerated 추가

--- src/main/java/com/prgrms/nabmart/domain/delivery/Delivery.java
+++ src/main/java/com/prgrms/nabmart/domain/delivery/Delivery.java
@@ -7,6 +7,8 @@ import com.prgrms.nabmart.domain.user.User;
 import com.prgrms.nabmart.global.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -42,6 +44,7 @@ public class Delivery extends BaseTimeEntity {
     @JoinColumn(name = "riderId")
     private Rider rider;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private DeliveryStatus deliveryStatus;
 
