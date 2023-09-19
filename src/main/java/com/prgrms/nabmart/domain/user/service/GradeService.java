0a1007c9040989fa019865a4f946dc8a43225be0
hseong3243 <hseong3243@gmail.com>
2023-09-19 17:12:45 +0900

    [NAYB-109] feat: 유저별 이전달 주문횟수 집계시 일괄조회로 변경

--- src/main/java/com/prgrms/nabmart/domain/user/service/GradeService.java
+++ src/main/java/com/prgrms/nabmart/domain/user/service/GradeService.java
@@ -5,7 +5,6 @@ import static java.util.stream.Collectors.groupingBy;
 import com.prgrms.nabmart.domain.user.UserGrade;
 import com.prgrms.nabmart.domain.user.repository.UserRepository;
 import com.prgrms.nabmart.domain.user.repository.response.UserOrderCount;
-import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
@@ -13,7 +12,6 @@ import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -22,30 +20,20 @@ import org.springframework.stereotype.Service;
 public class GradeService {
 
     private static final int ONE = 1;
-    private static final int PAGE_SIZE = 1000;
 
     private final UserRepository userRepository;
-    private final EntityManager entityManager;
 
     @Transactional
     public void updateUserGrade() {
-        long totalUserCount = userRepository.count();
-        long totalPage = totalUserCount / PAGE_SIZE;
         LocalDateTime startTimeOfPreviousMonth = getStartTimeOfPreviousMonth();
         LocalDateTime lastTimeOfPreviousMonth = getEndTimeOfPreviousMonth();
 
-        for (int page = 0; page <= totalPage; page++) {
-            PageRequest pageRequest = PageRequest.of(page, PAGE_SIZE);
-            List<UserOrderCount> userOrderCounts = userRepository.getUserOrderCount(
-                startTimeOfPreviousMonth,
-                lastTimeOfPreviousMonth,
-                pageRequest);
-            Map<UserGrade, List<UserOrderCount>> userGradeGroup = groupByUserGrade(userOrderCounts);
-            userGradeGroup.forEach(((userGrade, userOrderCountGroup) ->
-                userRepository.updateUserGrade(userGrade, extractUserIds(userOrderCountGroup))));
-
-            entityManager.clear();
-        }
+        List<UserOrderCount> userOrderCounts = userRepository.getUserOrderCount(
+            startTimeOfPreviousMonth,
+            lastTimeOfPreviousMonth);
+        Map<UserGrade, List<UserOrderCount>> userGradeGroup = groupByUserGrade(userOrderCounts);
+        userGradeGroup.forEach(((userGrade, userOrderCountGroup) ->
+            userRepository.updateUserGrade(userGrade, extractUserIds(userOrderCountGroup))));
     }
 
     private LocalDateTime getStartTimeOfPreviousMonth() {
